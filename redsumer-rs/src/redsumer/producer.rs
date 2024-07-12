faf0cab626eaa7e05b9ceb363046f9d221433f83
JMTamayo <jmtamayog23@gmail.com>
2024-07-11 21:52:01 -0500

    Implement ProducerConfig struct to manage configuration parameters. Implement ping to verify connection to server. Rename RedsumerProducer to Producer.  Implement ClientArgs and ProducerConfig as arguments in function Producer::new

--- redsumer-rs/src/redsumer/producer.rs
+++ redsumer-rs/src/redsumer/producer.rs
@@ -1,82 +1,106 @@
-use redis::{Client, Commands, ConnectionLike, ErrorKind, RedisError, ToRedisArgs};
+use redis::{Client, Commands, ToRedisArgs};
 
-use super::client::{get_redis_client, ClientCredentials};
+use super::client::{ping, ClientArgs, RedisClientBuilder};
 
 #[allow(unused_imports)]
 use super::types::{Id, RedsumerError, RedsumerResult};
 
-/// A producer implementation of *Redis Streams*.
-///
-///  This struct is responsible for producing messages in a stream.
-#[derive(Debug, Clone)]
-pub struct RedsumerProducer<'p> {
-    client: Client,
-    stream_name: &'p str,
+/// Define the configuration parameters to create a [`Producer`] instance.
+#[derive(Clone)]
+pub struct ProducerConfig<'d> {
+    stream_name: &'d str,
 }
 
-impl<'p> RedsumerProducer<'p> {
-    /// Get [`Client`].
-    fn get_client(&self) -> &Client {
-        &self.client
-    }
-
-    /// Get *stream name*.
+impl<'d> ProducerConfig<'d> {
+    /// Get **stream name**.
+    ///
+    /// # Arguments:
+    /// - No arguments.
+    ///
+    /// # Returns:
+    /// A reference of the stream name.
     pub fn get_stream_name(&self) -> &str {
         self.stream_name
     }
 
-    /// Build a new [`RedsumerProducer`] instance.
+    /// Create a new [`ProducerConfig`] instance.
+    ///
+    /// # Arguments:
+    /// - **stream_name**: The name of the stream where messages will be produced.
+    ///
+    /// # Returns:
+    /// A new [`ProducerConfig`] instance.
+    pub fn new(stream_name: &'d str) -> Self {
+        ProducerConfig { stream_name }
+    }
+}
+
+/// A producer implementation of *Redis Streams*.
+///
+///  This struct is responsible for producing messages in a specific stream.
+#[derive(Clone)]
+pub struct Producer<'p> {
+    client: Client,
+    config: ProducerConfig<'p>,
+}
+
+impl<'p> Producer<'p> {
+    /// Get [`Client`].
+    ///
+    /// # Arguments:
+    /// - No arguments.
+    ///
+    /// # Returns:
+    /// A reference to the [`Client`] parameters.
+    fn get_client(&self) -> &Client {
+        &self.client
+    }
+
+    /// Get *config*.
+    ///
+    /// # Arguments:
+    /// - No arguments.
+    ///
+    /// # Returns:
+    /// A reference to the [`ProducerConfig`] parameters.
+    pub fn get_config(&self) -> &ProducerConfig {
+        &self.config
+    }
+
+    /// Build a new [`Producer`] instance.
     ///
     /// Before creating a new producer, the following validations are performed:
     ///
-    /// - If connection string is invalid, a [`RedsumerError`] is returned.
+    /// - If connection parameters are invalid, a [`RedsumerError`] is returned.
     /// - If connection to Redis server can not be established, a [`RedsumerError`] is returned.
     ///
     /// # Arguments:
-    /// - **credentials**: Optional [`ClientCredentials`] to authenticate in Redis.
-    /// - **host**: Redis host.
-    /// - **port**: Redis port.
-    /// - **db**: Redis database.
-    /// - **stream_name**: Stream name to produce messages.
+    /// - **args**: A [`ClientArgs`] instance with the connection parameters to *Redis* server.
+    /// - **config**: A [`ProducerConfig`] instance with the producer configuration parameters.
     ///
     ///  # Returns:
-    ///  - A [`RedsumerResult`] with the new [`RedsumerProducer`] instance. Otherwise, a [`RedsumerError`] is returned.
+    ///  A [`RedsumerResult`] with the new [`Producer`] instance. Otherwise, a [`RedsumerError`] is returned.
     ///
     ///  # Example:
-    ///	Create a new [`RedsumerProducer`] instance.
+    ///	Create a new [`Producer`] instance.
     /// ```rust,no_run
-    /// use redsumer::RedsumerProducer;
+    /// use redsumer::{ClientArgs, Producer, ProducerConfig};
     ///
-    /// let producer: RedsumerProducer = RedsumerProducer::new(
-    ///     None,
-    ///     "localhost",
-    ///     "6379",
-    ///     "0",
-    ///     "my_stream",
+    /// let args: ClientArgs = ClientArgs::new("localhost");
+    /// let config: ProducerConfig = ProducerConfig::new("my_stream");
+    ///
+    /// let producer: Producer = Producer::new(
+    ///     args,
+    /// 	config,
     /// ).unwrap_or_else(|err| {
     ///    panic!("Error creating producer: {:?}", err);
     /// });
     /// ```
-    pub fn new(
-        credentials: Option<ClientCredentials<'p>>,
-        host: &'p str,
-        port: &'p str,
-        db: &'p str,
-        stream_name: &'p str,
-    ) -> RedsumerResult<RedsumerProducer<'p>> {
-        let mut client: Client = get_redis_client(credentials, host, port, db)?;
+    pub fn new(args: ClientArgs, config: ProducerConfig<'p>) -> RedsumerResult<Producer<'p>> {
+        let mut client: Client = args.build()?;
+        ping(&mut client)?;
 
-        if !client.check_connection() {
-            return Err(RedisError::from((
-                ErrorKind::TryAgain,
-                "Error getting connection to Redis server",
-            )));
-        };
-
-        Ok(RedsumerProducer {
-            client,
-            stream_name,
-        })
+        Ok(Producer { client, config })
     }
 
     /// Produce a new message in stream.
@@ -89,13 +113,13 @@ impl<'p> RedsumerProducer<'p> {
     /// - **message**: Message to produce in stream. It must implement [`ToRedisArgs`].
     ///
     ///  # Returns:
-    /// - A [`RedsumerResult`] with the *ID* of the produced message. Otherwise, a [`RedsumerError`] is returned.
+    /// A [`RedsumerResult`] with the [`Id`] of the produced message. Otherwise, a [`RedsumerError`] is returned.
     pub async fn produce<M>(&self, message: M) -> RedsumerResult<Id>
     where
         M: ToRedisArgs,
     {
         self.get_client().get_connection()?.xadd_map::<_, _, _, Id>(
-            self.get_stream_name(),
+            self.get_config().get_stream_name(),
             "*",
             message,
         )
