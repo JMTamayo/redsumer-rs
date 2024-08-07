544a1981e32bf86d483f5e75fa4e465bc9304544
JMTamayo <jmtamayog23@gmail.com>
2024-08-07 16:46:20 -0500

    Manual Debug implementation for ClientCredentials to hide password. Set function get_password as private. Implement unitary tests for Debug and Clone implementations. Implement unitary test for RedisClientBuilder with client credentials

--- redsumer-core/src/core/client.rs
+++ redsumer-core/src/core/client.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 use redis::Client;
 use redis::{ConnectionAddr, ConnectionInfo, RedisConnectionInfo};
 
@@ -5,7 +7,7 @@ use redis::{ConnectionAddr, ConnectionInfo, RedisConnectionInfo};
 use crate::types::{CommunicationProtocol, RedsumerError, RedsumerResult};
 
 /// To hold credentials to authenticate in Redis, that are used when server requires it.
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct ClientCredentials<'k> {
     user: &'k str,
     password: &'k str,
@@ -30,7 +32,7 @@ impl<'k> ClientCredentials<'k> {
     ///
     /// # Returns:
     /// The *password* to authenticate in Redis.
-    pub fn get_password(&self) -> &str {
+    fn get_password(&self) -> &str {
         self.password
     }
 
@@ -44,6 +46,15 @@ impl<'k> ClientCredentials<'k> {
     /// A new instance of [`ClientCredentials`].
     pub fn new(user: &'k str, password: &'k str) -> ClientCredentials<'k> {
         ClientCredentials { user, password }
+    }
+}
+
+impl<'k> Debug for ClientCredentials<'k> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ClientCredentials")
+            .field("user", &self.user)
+            .field("password", &"****")
+            .finish()
     }
 }
 
@@ -188,11 +199,11 @@ impl<'k, 'a> RedisClientBuilder for ClientArgs<'k, 'a> {
 }
 
 #[cfg(test)]
-mod test_client_parameters {
+mod test_client_credentials {
     use super::*;
 
     #[test]
-    fn test_client_credentials() {
+    fn test_client_credentials_builder_ok() {
         // Define the user and password to authenticate in Redis:
         let user: &str = "user";
         let password: &str = "password";
@@ -206,7 +217,48 @@ mod test_client_parameters {
     }
 
     #[test]
-    fn test_redis_client_args() {
+    fn test_client_credentials_debug() {
+        // Define the user and password to authenticate in Redis:
+        let user: &str = "user";
+        let password: &str = "password";
+
+        // Create a new instance of ClientCredentials:
+        let credentials: ClientCredentials = ClientCredentials::new(user, password);
+
+        // Verify if the debug is correct:
+        assert_eq!(
+            format!("{:?}", credentials),
+            "ClientCredentials { user: \"user\", password: \"****\" }"
+        );
+    }
+
+    #[test]
+    fn test_client_credentials_clone() {
+        // Define the user and password to authenticate in Redis:
+        let user: &str = "user";
+        let password: &str = "password";
+
+        // Create a new instance of ClientCredentials:
+        let credentials: ClientCredentials = ClientCredentials::new(user, password);
+
+        // Clone the credentials:
+        let cloned_credentials: ClientCredentials = credentials.clone();
+
+        // Verify if the credentials are correct:
+        assert_eq!(credentials.get_user(), cloned_credentials.get_user());
+        assert_eq!(
+            credentials.get_password(),
+            cloned_credentials.get_password()
+        );
+    }
+}
+
+#[cfg(test)]
+mod test_client_args {
+    use super::*;
+
+    #[test]
+    fn test_client_args_builder_ok() {
         // Define the user and password to authenticate in Redis:
         let user: &str = "user";
         let password: &str = "password";
@@ -240,6 +292,80 @@ mod test_client_parameters {
         assert_eq!(args.get_port(), port);
         assert_eq!(args.get_db(), db);
     }
+
+    #[test]
+    fn test_client_args_debug() {
+        // Define the user and password to authenticate in Redis:
+        let user: &str = "user";
+        let password: &str = "password";
+
+        // Create a new instance of ClientCredentials:
+        let credentials: ClientCredentials = ClientCredentials::new(user, password);
+
+        // Define the host to connect to Redis:
+        let host: &str = "localhost";
+
+        // Define the port to connect to Redis:
+        let port: u16 = 6379;
+
+        // Define the database to connect to Redis:
+        let db: i64 = 1;
+
+        // Define the redis protocol version:
+        let protocol_version: CommunicationProtocol = CommunicationProtocol::RESP2;
+
+        // Create a new instance of ClientArgs with default port and db:
+        let args: ClientArgs = ClientArgs::new(Some(credentials), host, port, db, protocol_version);
+
+        // Verify if the debug is correct:
+        assert_eq!(format!("{:?}", args), "ClientArgs { credentials: Some(ClientCredentials { user: \"user\", password: \"****\" }), host: \"localhost\", port: 6379, db: 1, protocol: RESP2 }");
+    }
+
+    #[test]
+    fn test_client_args_clone() {
+        // Define the user and password to authenticate in Redis:
+        let user: &str = "user";
+        let password: &str = "password";
+
+        // Create a new instance of ClientCredentials:
+        let credentials: ClientCredentials = ClientCredentials::new(user, password);
+
+        // Define the host to connect to Redis:
+        let host: &str = "localhost";
+
+        // Define the port to connect to Redis:
+        let port: u16 = 6379;
+
+        // Define the database to connect to Redis:
+        let db: i64 = 1;
+
+        // Define the redis protocol version:
+        let protocol_version: CommunicationProtocol = CommunicationProtocol::RESP2;
+
+        // Create a new instance of ClientArgs with default port and db:
+        let args: ClientArgs = ClientArgs::new(Some(credentials), host, port, db, protocol_version);
+
+        // Clone the args:
+        let cloned_args: ClientArgs = args.clone();
+
+        // Verify if the args are correct:
+        assert_eq!(
+            args.get_credentials().to_owned().unwrap().get_user(),
+            cloned_args.get_credentials().to_owned().unwrap().get_user()
+        );
+        assert_eq!(
+            args.get_credentials().to_owned().unwrap().get_password(),
+            cloned_args
+                .get_credentials()
+                .to_owned()
+                .unwrap()
+                .get_password()
+        );
+        assert_eq!(args.get_host(), cloned_args.get_host());
+        assert_eq!(args.get_port(), cloned_args.get_port());
+        assert_eq!(args.get_db(), cloned_args.get_db());
+        assert_eq!(args.get_protocol(), cloned_args.get_protocol());
+    }
 }
 
 #[cfg(test)]
@@ -247,10 +373,28 @@ mod test_redis_client_builder {
     use super::*;
 
     #[test]
-    fn test_redis_client_builder_ok() {
+    fn test_redis_client_builder_ok_with_null_credentials() {
         // Create a new instance of ClientArgs with default port and db:
         let args: ClientArgs =
             ClientArgs::new(None, "mylocalhost", 6377, 16, CommunicationProtocol::RESP2);
+
+        // Build a new instance of Client:
+        let client_result: RedsumerResult<Client> = args.build();
+
+        // Verify if the client is correct:
+        assert!(client_result.is_ok());
+    }
+
+    #[test]
+    fn test_redis_client_builder_ok_with_credentials() {
+        // Create a new instance of ClientArgs with default port and db:
+        let args: ClientArgs = ClientArgs::new(
+            Some(ClientCredentials::new("user", "password")),
+            "mylocalhost",
+            6377,
+            16,
+            CommunicationProtocol::RESP2,
+        );
 
         // Build a new instance of Client:
         let client_result: RedsumerResult<Client> = args.build();
